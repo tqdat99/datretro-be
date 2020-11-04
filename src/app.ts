7aa4949d22293ab59c3740804e4176ffab0acb16
tqdat99 <tqdat9@gmail.com>
2020-11-04 20:43:20 +0700

    Clean code
    Clean board.js card useEffect

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,10 @@ import config from "./config";
 import express from "express";
 import Logger from "./loaders/logger";
 
-async function startServer() {
-  const app = express();
+const app = express();
 
-  await require("./loaders").default({ expressApp: app });
+require("./loaders").default({ expressApp: app });
 
-  app.listen(config.port, err => {
-    if (err) {
-      Logger.error(err);
-      process.exit(1);
-    }
-    Logger.info(`Server is listening on port ${config.port}`);
-  });
-}
-
-startServer();
+app.listen(process.env.PORT || config.port, () => {
+  Logger.info(`Server is listening on port ${config.port}`);
+});
